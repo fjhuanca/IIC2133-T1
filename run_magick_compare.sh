ea27d89b78fea05e9beb09beca96b9f7a75f9f47
fjhuanca <fjhuanca@uc.cl>
2021-04-29 01:23:21 -0400

    Compare done

---
+++ run_magick_compare.sh
@@ -0,0 +1,34 @@
+mkdir differences
+mkdir differences/easy
+mkdir differences/medium
+mkdir differences/hard
+
+compare solutions/easy/test_1_area_5_60.png   results/easy/test_1_area_5_60.png   differences/easy/test_1_area_5_60.png 
+compare solutions/easy/test_1_delta_05.png    results/easy/test_1_delta_05.png    differences/easy/test_1_delta_05.png 
+compare solutions/easy/test_2_area_12_40.png  results/easy/test_2_area_12_40.png  differences/easy/test_2_area_12_40.png
+compare solutions/easy/test_2_area_50_5.png   results/easy/test_2_area_50_5.png   differences/easy/test_2_area_50_5.png 
+compare solutions/easy/test_2_delta_001.png   results/easy/test_2_delta_001.png   differences/easy/test_2_delta_001.png 
+compare solutions/easy/test_3_area_50_40.png  results/easy/test_3_area_50_40.png  differences/easy/test_3_area_50_40.png
+compare solutions/easy/test_3_delta_001.png   results/easy/test_3_delta_001.png   differences/easy/test_3_delta_001.png 
+compare solutions/easy/test_3_delta_01.png    results/easy/test_3_delta_01.png    differences/easy/test_3_delta_01.png 
+compare solutions/easy/test_4_area_18_30.png  results/easy/test_4_area_18_30.png  differences/easy/test_4_area_18_30.png 
+compare solutions/easy/test_4_delta_001.png   results/easy/test_4_delta_001.png   differences/easy/test_4_delta_001.png 
+
+
+compare solutions/medium/monalisa_area_5_30.png  results/medium/monalisa_area_5_30.png  differences/medium/monalisa_area_5_30.png
+compare solutions/medium/monalisa_delta_025.png  results/medium/monalisa_delta_025.png  differences/medium/monalisa_delta_025.png
+compare solutions/medium/moon_area_20_50.png     results/medium/moon_area_20_50.png     differences/medium/moon_area_20_50.png   
+compare solutions/medium/moon_delta_001.png      results/medium/moon_delta_001.png      differences/medium/moon_delta_001.png    
+compare solutions/medium/moon_delta_005.png      results/medium/moon_delta_005.png      differences/medium/moon_delta_005.png    
+
+
+compare solutions/hard/campus_area_200_0.png  results/hard/campus_area_200_0.png  differences/hard/campus_area_200_0.png
+compare solutions/hard/campus_delta_001.png   results/hard/campus_delta_001.png   differences/hard/campus_delta_001.png 
+compare solutions/hard/campus_delta_003.png   results/hard/campus_delta_003.png   differences/hard/campus_delta_003.png 
+compare solutions/hard/campus_delta_005.png   results/hard/campus_delta_005.png   differences/hard/campus_delta_005.png 
+
+compare solutions/hard/moon_area_500_7.png  results/hard/moon_area_500_7.png  differences/hard/moon_area_500_7.png
+compare solutions/hard/moon_delta_0008.png  results/hard/moon_delta_0008.png  differences/hard/moon_delta_0008.png
+
+compare solutions/hard/plate_area_100_60.png   results/hard/plate_area_100_60.png differences/hard/plate_area_100_60.png
+compare solutions/hard/plate_delta_0005.png    results/hard/plate_delta_0005.png  differences/hard/plate_delta_0005.png
